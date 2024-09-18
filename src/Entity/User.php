3571af812a11f3d4bc6b1e81ce8f5910b424f855
Salome <salome.burteaux.simplon@gmail.com>
2024-09-18 22:14:05 +0200

    crud ok

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -29,7 +29,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private array $roles = [];
-
+ 
     /**
      * @var string The hashed password
      */
